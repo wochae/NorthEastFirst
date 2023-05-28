e8770bc58bb81f924571b435e98b83e6d914de9f
wochae <officialwochae@gmail.com>
2023-05-28 22:21:00 +0900

    [feat] : post board impl #3

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,25 +1,32 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { BoardsService } from './boards.service';
 import { Board } from './board.model';
 
 @Controller('boards')
 export class BoardsController {
-    constructor(private readonly boardsService: BoardsService) {}
+    constructor(private readonly boardsService: BoardsService) { }
 
-/*
-    boardsService: BoardsService;
-
-    constructor(boardsService: BoardsService) {
-        this.boardsService = boardsService;
-    }
-
-*/
-    @Get()
+    /*
+        boardsService: BoardsService;
+    
+        constructor(boardsService: BoardsService) {
+            this.boardsService = boardsService;
+        }
+    
+    */
+    @Get('/')
     getBoards(): Board[] {
         return this.boardsService.getAllBoards();
     }
-/*
-    좌항 우항에 있는 boardsService는 서로 다른 객체이다. 
+    /*
+        좌항 우항에 있는 boardsService는 서로 다른 객체이다. 
+    */
 
-*/
+    @Post()
+    createBoard(
+        @Body('title') title: string,
+        @Body('description') description: string,
+    ): Board {
+        return this.boardsService.createBoard(title, description);
+    }
 }
