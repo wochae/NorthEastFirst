d362d5cffc10823b7be8a5ebfb8756c7950b0acd
wochae <officialwochae@gmail.com>
2023-05-29 13:04:27 +0900

    [feat] : parameter level pipe within custom pipes dir #5

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Delete, Get, Param, Patch, Post, UsePipes, Validation
 import { BoardsService } from './boards.service';
 import { Board, BoardStatus } from './board.model';
 import { CreateBoardDto } from './dto/create-board.dto';
+import { BoardStatusValidationPipe } from './pipes/board-status-validation.pipe';
 
 @Controller('boards')
 export class BoardsController {
@@ -42,8 +43,8 @@ export class BoardsController {
     @Patch('/:id/status')
     updateBoardStatus(
         @Param('id') id: string, 
-        @Body('status') status: BoardStatus
-    )   {
+        @Body('status', BoardStatusValidationPipe) status: BoardStatus
+    ): Board {
         return this.boardsService.updateBoardStatus(id, status);
     }
 }
