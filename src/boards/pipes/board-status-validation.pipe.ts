d0c14457b6c8c3adb52064bf6f1a186f49a0b242
wochae <officialwochae@gmail.com>
2023-05-29 13:13:28 +0900

    [feat] : imple update throw exception for #5

--- src/boards/pipes/board-status-validation.pipe.ts
+++ src/boards/pipes/board-status-validation.pipe.ts
@@ -1,11 +1,25 @@
-import { ArgumentMetadata, PipeTransform } from '@nestjs/common';
+import { ArgumentMetadata, BadRequestException, PipeTransform } from '@nestjs/common';
+import { BoardStatus } from '../board.model';
 
 export class BoardStatusValidationPipe implements PipeTransform {
     
-    transform(value: any, metadata: ArgumentMetadata) {
-        console.log('value', value);
-        console.log('metadata', metadata);
+    readonly StatusOptions = [
+        BoardStatus.PRIVATE,
+        BoardStatus.PUBLIC
+    ]
+
+    transform(value: any) {
+        value = value.toUpperCase();
+
+        if (!this.isStatusValid(value)) {
+            throw new BadRequestException(`${value} isn't in the status options`);
+        }      
         
         return value;
+    }
+
+    private isStatusValid(status: any) {
+        const index = this.StatusOptions.indexOf(status);
+        return index !== -1;
     }
 }
