d727b013ba92926421a61f07a8e439cb1a4da5df
wochae <officialwochae@gmail.com>
2023-05-31 17:20:51 +0900

    [fix] : In according to the docs, modifed for recent function param format #6

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -31,7 +31,7 @@ export class BoardsService {
     // }
 
     async getBoardById(id: number): Promise <Board> {
-        const found = await this.boardRepository.findOne(id);
+        const found = await this.boardRepository.findOneBy({id});
         
         if(!found) {
             throw new NotFoundException(`Can't find Board with id ${id}`);
