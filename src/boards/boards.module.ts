96fe09376b7901761462861f4b495998ec2dc0e7
wochae <officialwochae@gmail.com>
2023-05-31 16:29:52 +0900

    [feat] : BaseEntity, EntityRepository #6

--- src/boards/boards.module.ts
+++ src/boards/boards.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { BoardsService } from './boards.service';
 import { BoardsController } from './boards.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { BoardRepository } from './board.repository';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([BoardRepository])
+  ],
   controllers: [BoardsController],
   providers: [BoardsService]
 })
-export class BoardsModule {}
+export class BoardsModule { }
