560d853d7422e7618a5805504e98031b6e3d019a
wochae <officialwochae@gmail.com>
2023-05-31 20:33:58 +0900

    [fix] : found the answer on the config, it is the problem about entity not exist in module #6

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -1,4 +1,5 @@
 import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+import { Board } from "src/boards/board.entity";
 
 export const typeORMConfig : TypeOrmModuleOptions = {
     type: 'postgres',
@@ -7,6 +8,6 @@ export const typeORMConfig : TypeOrmModuleOptions = {
     username: 'postgres',
     password: 'postgres',
     database: 'board-app',
-    entities: [__dirname + '/../**/*.entity.{js,ts]'],
+    entities: [__dirname + '/../**/*.entity.{js,ts]', Board],
     synchronize: true
 }
