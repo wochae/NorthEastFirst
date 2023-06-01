ea50d4090cf3e6f30689b1339025416c2bd7835d
wochae <officialwochae@gmail.com>
2023-06-01 13:22:03 +0900

    [fix] : figured it out that' old version dependency issue #7

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,10 +1,13 @@
 import { Injectable } from "@nestjs/common";
-import { EntityRepository, Repository } from "typeorm";
+import { DataSource, Repository } from "typeorm";
 import { User } from "./user.entity";
 import { AuthCredentialsDto } from "./dto/auth-credential.dto";
 
 @Injectable()
 export class UserRepository extends Repository<User> {
+    constructor(private dataSource: DataSource) {
+        super(User, dataSource.createEntityManager());
+    }
     async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
         const { username, password } = authCredentialsDto;
 
