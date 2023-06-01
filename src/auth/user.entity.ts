89a71531d5bdf574c1103b235d1ad2c666b4f81b
wochae <officialwochae@gmail.com>
2023-06-01 13:38:14 +0900

    [feat] : this way that cause error 500, but not enough #7

--- src/auth/user.entity.ts
+++ src/auth/user.entity.ts
@@ -1,6 +1,7 @@
-import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn, Unique } from "typeorm";
 
 @Entity()
+@Unique(['username'])
 export class User extends BaseEntity {
     @PrimaryGeneratedColumn()
     id: number;
@@ -10,6 +11,5 @@ export class User extends BaseEntity {
 
     @Column()
     password: string;
-
 
 }
