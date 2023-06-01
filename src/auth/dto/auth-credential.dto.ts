acd04d0fbc4ef731a87da14369eb61f0bf3b953c
wochae <officialwochae@gmail.com>
2023-06-01 13:28:56 +0900

    [feat] : class-validator with regex #7

--- src/auth/dto/auth-credential.dto.ts
+++ src/auth/dto/auth-credential.dto.ts
@@ -1,4 +1,16 @@
+import { IsString, Matches, MaxLength, MinLength } from "class-validator";
+
 export class AuthCredentialsDto {
+    @IsString()
+    @MinLength(4)
+    @MaxLength(20)
     username: string;
+
+    @IsString()
+    @MinLength(4)
+    @MaxLength(20)
+
+    // 정규표현식
+    @Matches(/^[a-zA-Z0-9]*$/)
     password: string;
 }
