4f669b83aa9c45d6380b0e983048db55f908d54d
wochae <officialwochae@gmail.com>
2023-06-02 22:48:09 +0900

    [feat] : board domain grant for AuthGuard, Now GetAllBoard couldn't show everything #7

--- src/boards/boards.module.ts
+++ src/boards/boards.module.ts
@@ -3,9 +3,13 @@ import { BoardsService } from './boards.service';
 import { BoardsController } from './boards.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { BoardRepository } from './board.repository';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([BoardRepository])],
+  imports: [TypeOrmModule.forFeature([BoardRepository]),
+  AuthModule
+
+  ],
   controllers: [BoardsController],
   providers: [BoardsService, BoardRepository]
 })
