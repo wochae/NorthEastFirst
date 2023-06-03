ce6fcda7bf95da775e381410ccce38665d770edf
wochae <officialwochae@gmail.com>
2023-06-03 19:37:01 +0900

    [feat] : Impl Post a Board with User's Auth #8

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -5,6 +5,8 @@ import { CreateBoardDto } from './dto/create-board.dto';
 import { BoardStatusValidationPipe } from './pipes/board-status-validation.pipe';
 import { Board } from './board.entity';
 import { AuthGuard } from '@nestjs/passport';
+import { GetUser } from 'src/auth/get-user.decorator';
+import { User } from 'src/auth/user.entity';
 
 @Controller('boards')
 @UseGuards(AuthGuard())
@@ -41,8 +43,11 @@ export class BoardsController {
     
     @Post()
     @UsePipes(ValidationPipe)
-    createBoard(@Body() createBoardDto: CreateBoardDto): Promise<Board> {
-        return this.boardsService.createBoard(createBoardDto);
+    createBoard(
+        @Body() createBoardDto: CreateBoardDto,
+        @GetUser() user:User,
+        ): Promise<Board> {
+        return this.boardsService.createBoard(createBoardDto, user);
     }
 
     @Get('/:id')
