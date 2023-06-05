4c058003885340ef5a706af8fdc3046158e84070
wochae <officialwochae@gmail.com>
2023-06-05 20:13:31 +0900

    [feat] : printing id and check whether if to be connected or not #13

--- src/gateway/gateway.ts
+++ src/gateway/gateway.ts
@@ -1,11 +1,18 @@
+import { OnModuleInit } from "@nestjs/common";
 import { MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
 import { Server } from "socket.io";
 
 @WebSocketGateway()
-export class Mygateway {
-
+export class Mygateway implements OnModuleInit {
     @WebSocketServer()
     server : Server;
+
+    onModuleInit() {
+        this.server.on('connection', (socket) => {
+            console.log(socket.id);
+            console.log('Connected');
+        });
+    }
     @SubscribeMessage('newMessage')
     onNewMessage(@MessageBody() body: any) {
         console.log(body);
