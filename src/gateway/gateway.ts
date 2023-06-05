c51b3dd033f5e697965e4be66fd7c8774e259135
wochae <officialwochae@gmail.com>
2023-06-05 20:07:45 +0900

    [feat] : Impl WebSocketServer deco, Not even using onModuleInit yet #13

--- src/gateway/gateway.ts
+++ src/gateway/gateway.ts
@@ -1,11 +1,13 @@
-import { MessageBody, SubscribeMessage, WebSocketGateway } from "@nestjs/websockets";
-
+import { MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
+import { Server } from "socket.io";
 
 @WebSocketGateway()
 export class Mygateway {
 
+    @WebSocketServer()
+    server : Server;
     @SubscribeMessage('newMessage')
     onNewMessage(@MessageBody() body: any) {
         console.log(body);
     }
-}
+ }
